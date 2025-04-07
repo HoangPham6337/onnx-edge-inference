eeb228660706f12afa463577c8a2c7635802961d
HoangPham4171 <hoangpham4171@gmail.com>
2025-04-07 14:48:58 +0200

    New modular pipeline

--- benchmark.py
+++ benchmark.py
@@ -8,53 +8,16 @@ import numpy as np
 import onnxruntime as ort
 import scipy.special
 from tqdm import tqdm
+from utility import measure_inference_memory, preprocess_eval_opencv
 
 from dataset_builder.core.utility import load_manifest_parquet
 
 OUTPUT_FILE = "inference_results.csv"
-MODEL_PATH = "./models/InceptionV3_HG_onnx/inceptionv3_50.onnx"
-# INPUT_SIZE = (224, 224)
-INPUT_SIZE = (299, 299)
+MODEL_PATH = "/mnt/ssd/onnx-edge-inference/models/MobileNetV3-modified_train_prune_retrain/mobilenet_v3_large_90_pruned_train.onnx"
+INPUT_SIZE = (224, 224)
+# INPUT_SIZE = (299, 299)
 LIMIT = 100
 print(MODEL_PATH)
-
-peak_mem_mb = 0
-def monitor_memory(pid):
-    global peak_mem_mb
-    proc = psutil.Process(pid)
-    while True:
-        try:
-            mem = proc.memory_info().rss / 1024 ** 2
-            peak_mem_mb = max(peak_mem_mb, mem)
-            time.sleep(0.5)
-        except psutil.NoSuchProcess:
-            break
-
-
-
-def preprocess_eval_opencv(image_path, width, height, central_fraction=0.857):
-    img = cv2.imread(image_path)
-    if img is None:
-        raise ValueError("Failed to read image")
-    h, w, _ = img.shape
-
-    crop_h = int(h * central_fraction)
-    crop_w = int(w * central_fraction)
-
-    offset_h = (h - crop_h) // 2
-    offset_w = (w - crop_w) // 2
-    img = img[offset_h: offset_h + crop_h, offset_w: offset_w + crop_w]
-
-    img = cv2.resize(img, (width, height), interpolation=cv2.INTER_LINEAR)
-
-    img = img.astype(np.float32) / 255.0
-    img = (img - 0.5) * 2.0
-
-    img = np.expand_dims(img, axis=0)
-    # img = np.transpose(img, (0, 3, 1, 2))
-
-    return img
-
 
 print(ort.get_available_providers())
 def run_inference_benchmark():
@@ -68,27 +31,25 @@ def run_inference_benchmark():
     input_dtype = session.get_inputs()[0].type
     print(f"Input: {input_name}, Shape: {input_shape}, Dtype: {input_dtype}")
 
-    data_manifest = load_manifest_parquet("./data/haute_garonne/dataset_manifest.parquet")[0:1000]
+    data_manifest = load_manifest_parquet("./data/haute_garonne/dataset_manifest.parquet")[0:100]
 
     with open(OUTPUT_FILE, "w") as csv_file:
-        csv_file.write("filepath,predicted_species,probability,inference_time_s,correct_species\n")
+        csv_file.write("filepath,predicted_species,probability,inference_time_s,correct_species,peak_mem_mb\n")
 
         loop = tqdm(data_manifest, desc="Image")
         for image_data in loop:
             image_path = image_data[0]
+            correct_species = image_data[1]
             try:
                 img = preprocess_eval_opencv(image_path, *INPUT_SIZE)
 
-                start = time.perf_counter()
-                outputs = session.run(None, {input_name: img})
-                end = time.perf_counter()
+                outputs, inference_time, peak_mem = measure_inference_memory(session, input_name, img)
 
                 probabilities = scipy.special.softmax(outputs[0], axis=1)
                 top1_idx = int(np.argmax(probabilities[0]))
                 top1_prob = float(probabilities[0][top1_idx])
-                inference_time = end - start
 
-                csv_file.write(f"{image_path},{top1_idx},{top1_prob: .4f},{inference_time: .5f}\n")
+                csv_file.write(f"{image_path},{top1_idx},{top1_prob: .4f},{inference_time: .5f},{correct_species},{peak_mem}\n")
                 csv_file.flush()
             except Exception as e:
                 print(f"ERROR Skipping {image_path}: {e}")
@@ -96,14 +57,10 @@ def run_inference_benchmark():
 if __name__ == "__main__":
     print(f"PID: {os.getpid()}")
     print("READY_FOR_PERF",flush=True)
-    # time.sleep(2)
     input("Attach `perf` now and press Enter to begin inference ...")
-    monitor_thread = threading.Thread(target=monitor_memory, args=(os.getpid(),), daemon=True)
-    monitor_thread.start()
 
     start_time = time.time()
     run_inference_benchmark()
     end_time = time.time()
 
     print(f"\nTotal inference time: {end_time - start_time:.2f}s")
-    print(f"Peak memory during inference: {peak_mem_mb:.2f} MB")
--- data_analyzer_optimize.py
+++ data_analyzer_optimize.py
@@ -4,7 +4,7 @@ import json
 import math
 
 
-INFER_RESULT_PATH = "./inference_results.csv"
+INFER_RESULT_PATH = "inference_results_mobilenetv3_train_prune_retrain_50.csv"
 
 df = pd.read_csv(INFER_RESULT_PATH)
 other_class_id = 16
@@ -19,24 +19,24 @@ num_local = df["is_local"].sum()
 num_comm = df["is_communication"].sum()
 num_total = len(df)
 
-# accuracy = np.mean(df["correct_species"] == df["predicted_species"])
+accuracy = np.mean(df["correct_species"] == df["predicted_species"])
 
-# df["is_other"] = df["predicted_species"] == other_class_id
-# num_other_outputs = df["is_other"].sum()
-# num_hits = (df["correct_species"] == df["predicted_species"]).sum()
-# num_misses = num_total - num_hits
-# hit_to_miss_ratio = num_hits / max(num_misses, 1)
+df["is_other"] = df["predicted_species"] == other_class_id
+num_other_outputs = df["is_other"].sum()
+num_hits = (df["correct_species"] == df["predicted_species"]).sum()
+num_misses = num_total - num_hits
+hit_to_miss_ratio = num_hits / max(num_misses, 1)
 
-# inference_times = df["inference_time_s"].astype(float)
-# std_inference_time = np.std(inference_times)
-# avg_inference_time = np.mean(inference_times)
+inference_times = df["inference_time_s"].astype(float)
+std_inference_time = np.std(inference_times)
+avg_inference_time = np.mean(inference_times)
 
 
 print(f"Total inference: {num_total}")
 
 print(f"Local (non-Other): {num_local} ({(num_local / num_total) * 100:.2f}%)")
 print(f"Communication (Other): {num_comm} ({(num_comm / num_total) * 100:.2f}%)")
-# print(f"Accuracy: {accuracy * 100:.2f}%")
+print(f"Accuracy: {accuracy * 100:.2f}%")
 # print(f"'Other' predictions: {num_other_outputs}/{num_total} ({(num_other_outputs / num_total) * 100:.2f}%)")
 # print(f"Hit/Miss ratio: ({num_hits}/{max(num_misses, 1)}) {hit_to_miss_ratio:.2f}%")
 # print(f"Average inference time: {avg_inference_time:.2f}s ~ {math.floor(1 / avg_inference_time)} inference/second")
